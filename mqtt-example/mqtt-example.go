0abf579fda60c775c49b856373c485129f143de3
PRASANNA ALUR MATHADA <amprasanna@in.ibm.com>
2017-08-28 17:43:44 +0530

    updated files

--- mqtt-example/mqtt-example.go
+++ mqtt-example/mqtt-example.go
@@ -1,26 +1,28 @@
 package main
 
 import (
-  "fmt"
-  //import the Paho Go MQTT library
-  MQTT "github.com/eclipse/paho.mqtt.golang"
-  "time"
+	"fmt"
+	MQTT "github.com/eclipse/paho.mqtt.golang"
+	"code.cloudfoundry.org/cli/plugin"
+	"time"
 )
 
-func main() {
-  //create a ClientOptions struct setting the broker address, clientid, turn
-  //off trace output and set the default message handler
+type BasicPlugin struct{}
+
+func (c *BasicPlugin) Run(cliConnection plugin.CliConnection, args []string) {
+
+	if args[0] == "wiotp" {
+		fmt.Println("Connected to Watson IoT Platform ")
   opts := MQTT.NewClientOptions().AddBroker("tcp://quickstart.messaging.internetofthings.ibmcloud.com:1883")
   opts.SetClientID("d:quickstart:device:112233445566")
 
-  //create and start a client using the above ClientOptions
   c := MQTT.NewClient(opts)
   if token := c.Connect(); token.Wait() && token.Error() != nil {
     panic(token.Error())
   }
 
 
-    text := fmt.Sprintf("{ \"d\" : { \"temp\" : 44 }}")
+    text := fmt.Sprintf("{ \"d\" : { \"temp\" : 24 }}")
     token := c.Publish("iot-2/evt/stat/fmt/json", 0, false, text)
     token.Wait()
 
@@ -29,4 +31,37 @@ func main() {
 
 
   c.Disconnect(250)
+	}
+}
+
+func (c *BasicPlugin) GetMetadata() plugin.PluginMetadata {
+	return plugin.PluginMetadata{
+		Name: "MyBasicPlugin",
+		Version: plugin.VersionType{
+			Major: 1,
+			Minor: 0,
+			Build: 0,
+		},
+		MinCliVersion: plugin.VersionType{
+			Major: 6,
+			Minor: 7,
+			Build: 0,
+		},
+		Commands: []plugin.Command{
+			{
+				Name:     "wiotp",
+				HelpText: "Basic plugin command's help text",
+
+				// UsageDetails is optional
+				// It is used to show help of usage of each command
+				UsageDetails: plugin.Usage{
+					Usage: "basic-plugin-command\n   cf basic-plugin-command",
+				},
+			},
+		},
+	}
+}
+
+func main() {
+	plugin.Start(new(BasicPlugin))
 }
